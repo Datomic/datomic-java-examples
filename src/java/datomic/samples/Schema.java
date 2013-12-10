70deb796e44a332ff4ccb300116c24197823dda0
rondagostino <rndgstn@gmail.com>
2013-12-10 00:36:28 -0500

    First version that seems to completely work

--- src/java/datomic/samples/Schema.java
+++ src/java/datomic/samples/Schema.java
@@ -2,6 +2,7 @@ package datomic.samples;
 
 import static datomic.Peer.q;
 import static datomic.Util.list;
+import static datomic.Util.map;
 import static datomic.Util.read;
 import static datomic.samples.Fns.scratchConnection;
 import static datomic.samples.Fns.solo;
@@ -9,6 +10,7 @@ import static datomic.samples.Fns.solo;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,11 +18,12 @@ import java.util.concurrent.ExecutionException;
 
 import datomic.Connection;
 import datomic.Database;
-import datomic.Entity;
 import datomic.Peer;
 import datomic.samples.Schema.Element.Handler;
 
 public class Schema {
+	public static final String DEFAULT_SCHEMA_TX_ATTRIB_IDENT = ":schemaTx/schemaName";
+
 	public static abstract class Element {
 		public static class Handler {
 			public Handler() {	
@@ -135,7 +138,7 @@ public class Schema {
 
 		@Override
 		public String toString() {
-			return toEdnList();
+			return toEdnMap();
 		}
 
 		public Map<String, Object> toMap() {
@@ -400,7 +403,7 @@ public class Schema {
 		}
 
 		public boolean exists(final Database db) {
-			return Schema.hasAttribute(db, getIdent());
+			return Schema.hasAttrib(db, getIdent());
 		}
 		
 		public Attrib component() {
@@ -418,7 +421,7 @@ public class Schema {
 
 		public Attrib doc(final String doc) {
 			final Attrib retval = new Attrib(this);
-			retval.doc = doc;
+			retval.doc = "\"" + doc + "\"";
 			return retval;
 		}
 
@@ -669,48 +672,68 @@ public class Schema {
                            "[?card :db/ident ?v]]", db, attr)));
     }
 
-    public static boolean hasAttribute(Object db, String attrName) {
-    	Entity entity = ((Database)db).entity(attrName);
-		return entity.get(":db.install/_attribute") != null;
+    public static boolean hasAttrib(final Database db, final String ident) {
+    	return db.entity(ident).get(":db.install/_attribute") != null;
     }
     
-    public static boolean hasPartition(Object db, String partitionName) {
+    public static boolean hasPartition(final Database db, final String ident) {
     	// the linkage for db.part/user and db.part/tx is not explicit
     	// as per https://groups.google.com/d/msg/datomic/9xxoWH6RxhA/AYVObmrhBp0J
-    	if (":db.part/user".equals(partitionName) || "db.part/user".equals(partitionName) ||
-    		":db.part/tx".equals(partitionName) || "db.part/tx".equals(partitionName)) {
+    	if (":db.part/user".equals(ident) || "db.part/user".equals(ident) ||
+    		":db.part/tx".equals(ident) || "db.part/tx".equals(ident)) {
     		return true;
     	}
     	return !q("[:find ?p " +
                    ":in $ ?partitionName " +
     			   ":where [_ :db.install/partition ?p] [?p :db/ident ?partitionName]]",
-    			   db, partitionName)
+    			   db, ident)
     			.isEmpty();
     }
     
-    public static boolean hasEnumEntity(Object db, String ident) {
-    	Entity entity = ((Database)db).entity(ident);
-		return entity.get(":db/id") != null;
+    public static boolean hasEnumEntity(final Database db, final String ident) {
+    	return db.entity(ident).get(":db/id") != null;
     }
 
-    public static boolean hasSchema(Object db, String schemaAttrName, String schemaName) {
-    	return hasAttribute(db, schemaAttrName) &&
-    		!q("[:find ?e " +
-                ":in $ ?sa ?sn " +
-                ":where [?e ?sa ?sn]]", db, schemaAttrName, schemaName).isEmpty();        
-    }    
+    public static boolean hasSchema(final Database db, final String schemaName) {
+    	return hasSchema(db, DEFAULT_SCHEMA_TX_ATTRIB_IDENT, schemaName);
+    }
 
-    public static void ensurePartition(final Connection conn, final String partitionName) {
+    public static boolean hasSchema(final Database db, final String schemaTxAttribIdent, final String schemaName) {
+		return hasAttrib(db, schemaTxAttribIdent) &&
+				!q("[:find ?tx " +
+					":in $ ?schemaTxAttribIdent ?schemaName " +
+					":where [?tx ?schemaTxAttribIdent ?schemaName] [?tx :db/txInstant]]",
+						db, schemaTxAttribIdent, schemaName).isEmpty();
+    }
+
+    @SuppressWarnings("unchecked")
+	public static void ensurePartition(final Connection conn, final String partitionName) {
     	if (!hasPartition(conn.db(), partitionName)) {
     		try {
-				conn.transact(list(new Partition(partitionName).toMap())).get();
+    			Schema.transact(conn, list(list(new Partition(partitionName))));
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
     	}
     }
 
-    public static void transact(final Connection conn, final List<List<Element>> elementTxs) throws InterruptedException, ExecutionException {
+	public static void ensureSchema(final Connection conn, final String schemaName, final List<List<Element>> txData) throws InterruptedException, ExecutionException {
+		ensureSchema(conn, DEFAULT_SCHEMA_TX_ATTRIB_IDENT, schemaName, txData);
+	}
+	
+	public static void ensureSchema(final Connection conn, final String schemaTxAttribIdent, final String schemaName,
+			final List<List<Element>> txData) throws InterruptedException, ExecutionException {
+		if (txData == null || txData.size() == 0 || txData.iterator().next().size() == 0) {
+			throw new RuntimeException("No transaction data for schema name: " + schemaName);
+		}
+		ensureSchemaTxAttrib(conn, schemaTxAttribIdent);
+		Schema.transact(conn, txData);
+		conn.transact(list(
+				map(":db/id", Peer.tempid(":db.part/tx"), schemaTxAttribIdent, schemaName),
+				map(":db/id", Peer.tempid(":db.part/user")))).get();
+	}
+
+	public static void transact(final Connection conn, final List<List<Element>> elementTxs) throws InterruptedException, ExecutionException {
     	transact(conn, elementTxs, new Handler());
     }
 
@@ -753,12 +776,18 @@ public class Schema {
     	return sb.toString();
     }
 
-    private static void ensureSchemaAttribute(Object conn, String schemaAttrName) {
-    	if (!hasAttribute(((Connection)conn).db(), schemaAttrName)) {
+	private static void ensureSchemaTxAttrib(final Connection conn) {
+		ensureSchemaTxAttrib(conn, DEFAULT_SCHEMA_TX_ATTRIB_IDENT);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void ensureSchemaTxAttrib(final Connection conn, final String schemaTxAttribIdent) {
+    	if (!hasAttrib(conn.db(), schemaTxAttribIdent)) {
     		try {
-				((Connection)conn).transact(list(keywordAttrib(schemaAttrName)
-						.doc("Name of schema installed by this transaction")
-						.index().toMap())).get();
+    			Schema.transact(conn, list(list(
+    					stringAttrib(schemaTxAttribIdent)
+							.doc("Name of schema installed by a transaction")
+							.index())));
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
@@ -807,66 +836,82 @@ public class Schema {
         System.out.println(hasPartition(db, ":foo"));
 
         // generate the seattle schema
-        final List<Element> elements =  new ArrayList<Element>();
+        // put it in its own partition just for demo purposes
+        final List<Element> partitionTxData =  new ArrayList<Element>();
+        final Partition geographyPartition = new Partition(":geography");
+        partitionTxData.add(geographyPartition);
+        final List<Element> elementsTxData =  new ArrayList<Element>();
         //
-        elements.add(comment(";; community"));
-        elements.add(stringAttrib(":community/name").fulltext().doc("A community's name"));
-        elements.add(stringAttrib(":community/url").doc("A community's url"));
-        elements.add(refAttrib(":community/neighborhood").doc("A community's neighborhood"));
-        elements.add(manyStringsAttrib(":community/category").fulltext().doc("All community categories"));
-        elements.add(refAttrib(":community/orgtype").doc("A community orgtype enum value"));
-        elements.add(refAttrib(":community/type").doc("A community type enum value"));
+        elementsTxData.add(comment(";; community"));
+        elementsTxData.add(stringAttrib(":community/name").fulltext().doc("A community's name"));
+        elementsTxData.add(stringAttrib(":community/url").doc("A community's url"));
+        elementsTxData.add(refAttrib(":community/neighborhood").doc("A community's neighborhood"));
+        elementsTxData.add(manyStringsAttrib(":community/category").fulltext().doc("All community categories"));
+        elementsTxData.add(refAttrib(":community/orgtype").doc("A community orgtype enum value"));
+        elementsTxData.add(refAttrib(":community/type").doc("A community type enum value"));
         //
-        elements.add(comment(";; community/orgtype enum values"));
-        elements.add(enumEntity(":community.orgtype/community"));
-        elements.add(enumEntity(":community.orgtype/commercial"));
-        elements.add(enumEntity(":community.orgtype/nonprofit"));
-        elements.add(enumEntity(":community.orgtype/personal"));
+        elementsTxData.add(comment(";; community/orgtype enum values"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.orgtype/community"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.orgtype/commercial"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.orgtype/nonprofit"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.orgtype/personal"));
         //
-        elements.add(comment(";; community/type enum values"));
-        elements.add(enumEntity(":community.type/email-list"));
-        elements.add(enumEntity(":community.type/twitter"));
-        elements.add(enumEntity(":community.type/facebook-page"));
-        elements.add(enumEntity(":community.type/blog"));
-        elements.add(enumEntity(":community.type/website"));
-        elements.add(enumEntity(":community.type/wiki"));
-        elements.add(enumEntity(":community.type/myspace"));
-        elements.add(enumEntity(":community.type/ning"));
+        elementsTxData.add(comment(";; community/type enum values"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/email-list"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/twitter"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/facebook-page"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/blog"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/website"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/wiki"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/myspace"));
+        elementsTxData.add(geographyPartition.enumEntity(":community.type/ning"));
         //
-        elements.add(comment(";; neighborhood"));
-        elements.add(stringAttrib(":neighborhood/name").uniqueIdentity().doc("A unique neighborhood name (upsertable)"));
-        elements.add(refAttrib(":neighborhood/district").uniqueIdentity().doc("A neighborhood's district"));
+        elementsTxData.add(comment(";; neighborhood"));
+        elementsTxData.add(stringAttrib(":neighborhood/name").uniqueIdentity().doc("A unique neighborhood name (upsertable)"));
+        elementsTxData.add(refAttrib(":neighborhood/district").uniqueIdentity().doc("A neighborhood's district"));
         //
-        elements.add(comment(";; district"));
-        elements.add(stringAttrib(":district/name").uniqueIdentity().doc("A unique district name (upsertable)"));
-        elements.add(refAttrib(":district/region").doc("A district region enum value"));
+        elementsTxData.add(comment(";; district"));
+        elementsTxData.add(stringAttrib(":district/name").uniqueIdentity().doc("A unique district name (upsertable)"));
+        elementsTxData.add(refAttrib(":district/region").doc("A district region enum value"));
         //
-        elements.add(comment(";; district/region enum values"));
-        elements.add(enumEntity(":region/n"));
-        elements.add(enumEntity(":region/ne"));
-        elements.add(enumEntity(":region/e"));
-        elements.add(enumEntity(":region/se"));
-        elements.add(enumEntity(":region/s"));
-        elements.add(enumEntity(":region/sw"));
-        elements.add(enumEntity(":region/w"));
-        elements.add(enumEntity(":region/nw"));
-        System.out.println("Here is what the list of elements looks like when you invoke toString():");
-        System.out.println(elements.toString());
-        final String edn = Schema.toEdn(list(elements));
-        System.out.println("Here is what the list of elements looks like when you convert it to edn:");
-		System.out.println(edn);
-        Schema.transact(conn, list(elements));
+        elementsTxData.add(comment(";; district/region enum values"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/n"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/ne"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/e"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/se"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/s"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/sw"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/w"));
+        elementsTxData.add(geographyPartition.enumEntity(":region/nw"));
+        System.out.println("Here is what the transaction of (a single) partition looks like when you invoke toString():");
+        System.out.println(partitionTxData.toString());
+        System.out.println("Here is what the list of elements in a transaction looks like when you invoke toString():");
+        System.out.println(elementsTxData.toString());
+        final List txData = list(partitionTxData, elementsTxData);
+		final String txsEdn = Schema.toEdn(txData);
+        System.out.println("Here is what the two transactions look like when you convert them to edn:");
+		System.out.println(txsEdn);
+        System.out.println("Has schema?");
+        final String schemaName = "geography";
+		System.out.println(Schema.hasSchema(conn.db(), schemaName));
+        Schema.transact(conn, txData);
         // do it again to make sure the edn is valid
-        IO.transactAll(conn, new StringReader(edn));
+        IO.transactAll(conn, new StringReader(txsEdn));
+        System.out.println("Has schema?");
+        System.out.println(Schema.hasSchema(conn.db(), schemaName));
+        // do it a final time to mark the schema as being installed
+        Schema.ensureSchema(conn, schemaName, txData);
+        System.out.println("Has schema?");
+        System.out.println(Schema.hasSchema(conn.db(), schemaName));
         db = conn.db(); // get latest version of the database so we see the change
         System.out.println("Database has a :community/name attribute?");
-        System.out.println(hasAttribute(db, ":community/name") ?
+        System.out.println(hasAttrib(db, ":community/name") ?
         	"true; cardinality = " + cardinality(db, ":community/name") : false);
         System.out.println("Database has a :community/category attribute?");
-        System.out.println(hasAttribute(db, ":community/category") ?
+        System.out.println(hasAttrib(db, ":community/category") ?
         	"true; cardinality = " + cardinality(db, ":community/category") : false);
         System.out.println("Database has a :bar attribute?");
-        System.out.println(hasAttribute(db, ":bar") ?
+        System.out.println(hasAttrib(db, ":bar") ?
         	"true; cardinality = " + cardinality(db, ":bar") : false);
         System.out.println("Database has a :community.orgtype/community enum entity?");
         System.out.println(hasEnumEntity(db, ":community.orgtype/community"));
