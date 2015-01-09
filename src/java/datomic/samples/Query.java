2080db7f3c3ab2323c31af1b0a32d1e1bda0584e
Ben Kamphaus <ben.kamphaus@gmail.com>
2015-01-09 13:54:23 -0700

    incremental check-in of query port

--- src/java/datomic/samples/Query.java
+++ src/java/datomic/samples/Query.java
@@ -3,6 +3,7 @@ package datomic.samples;
 import datomic.Connection;
 import datomic.Database;
 import datomic.Peer;
+import datomic.Util;
 
 import java.util.Scanner;
 import java.util.Collection;
@@ -28,6 +29,43 @@ public class Query {
         Collection results = Peer.query("[:find ?release-name :where [_ :release/name ?release-name]]", db);
         System.out.println(results);
         pause();
+
+        System.out.println("Same as previous query, but with explicit db-var ($).");
+        results = Peer.query("[:find ?release-name :in $ :where [$ _ :release/name ?release-name]]", db);
+        System.out.println(results);
+        pause();
+
+        System.out.println("Query that finds releases by artist. Parameterized to accept artist name as an argument.");
+        results = Peer.query("[:find ?release-name " +
+                              ":in $ ?artist-name " +
+                              ":where [?artist :artist/name ?artist-name] " +
+                                     "[?release :release/artists ?artist] " +
+                                     "[?release :release/name ?release-name]]",
+                            db, "John Lennon");
+        System.out.println(results);
+        pause();
+
+        System.out.println("This query accepts a relation argument as a vector and destructures it within the :in.");
+        results = Peer.query("[:find ?release " +
+                             " :in $ [?artist-name ?release-name] " +
+                             " :where [?artist :artist/name ?artist-name] " +
+                             "        [?release :release/artists ?artist] " +
+                             "        [?release :release/name ?release-name]]",
+                             db, Util.list("John Lennon", "Mind Games"));
+        System.out.println(results);
+        pause();
+
+
+        System.out.println("This query accepts a list of artist arguments and uses destructuring to satisfy the " +
+                           "query for each of them (union of results).");
+        results = Peer.query("[:find ?release-name " +
+                              ":in $ [?artist-name ...] " +
+                              ":where [?artist :artist/name ?artist-name] " +
+                              "       [?release :release/artists ?artist] " +
+                              "       [?release :release/name ?release-name]]",
+                              db, Util.list("Paul McCartney", "George Harrison"));
+        pause();
+                              
 
         System.out.println("Total number of artists without :artist/country attribute.");
         Integer res = Peer.query("[:find (count ?eid) . " +
